748f245812bde51a1fe53fd9e8c32507489bf75f
Hassan Shamim <hassan.has.you@gmail.com>
2016-12-16 04:23:40 -0800

    show location page

--- emergency/emergency/urls.py
+++ emergency/emergency/urls.py
@@ -21,4 +21,5 @@ from maps import views as map_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^map/', map_views.index, name='map'),
+    url(r'^location/(?P<location_id>[0-9]+)', map_views.show_location, name='location'),
 ]
--- emergency/maps/views.py
+++ emergency/maps/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Location
 
 
 def index(request):
     return HttpResponse('Hello from maps index')
+
+def show_location(request, location_id):
+    loc = get_object_or_404(Location, pk=location_id)
+    # render(location_template, loc)
+    return render(request, 'maps/show_location.html', {'location': loc})
